3f9f7b10a3324fb17cdbe9dc77f8583ca3fd50bb
Sanjam <sanjam1552@gmail.com>
2026-07-08 13:10:54 +0530

    update presentation assets

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // RVNL Creative & PR Reporting Hub - Core Application Logic
-const APP_VERSION = "1.2.0";
+const APP_VERSION = "1.1.0";
 
 // Format current date to Month Year (e.g. "June 2026")
 function getCurrentMonthStr() {
